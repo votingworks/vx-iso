29109be4515a94609216da71c3bb7b6fd4d46401
Matt Bernhard <mdb92nc@gmail.com>
2022-04-25 13:57:49 -0400

    fixing a typo

--- airootfs/usr/share/vx-img/flash-image.sh
+++ airootfs/usr/share/vx-img/flash-image.sh
@@ -68,20 +68,23 @@ done
 
 echo "Extracting and flashing $_toflash"
 
-if [ $_compression == "gzip" ]; then
+if [[ $_compression == "gzip" ]]; then
     _finalsize=$($_compression -l | tail -n 1 | sed 's/\s\+[0-9]\+\s\+\([0-9]\+\).*/\1/')
 else
     echo "lz4 doesn't support storing the uncompressed data size in the compressed file. Please enter the uncompressed size (default is 64g):"
     read _finalsize
 fi
 
-$_compression -c -d $_path/$_filename | pv -s $_finalsize > /dev/nvme0n1
+echo "would have run..."
+echo "$_compression -c -d $_path/$_filename | pv -s $_finalsize > /dev/nvme0n1"
+exit
 
-echo "Now checking that the write was successful."
-echo "The hash should be:"
-cat /usr/share/vx-img/image.sha256sum 
 
 if [ $_hashash == 1 ]; then 
+    echo "Now checking that the write was successful."
+    echo "The hash should be:"
+    cat /usr/share/vx-img/image.sha256sum 
+
     echo "Computing hash..."
     head -c $_finalsize /dev/nvme0n1 | pv -s $_finalsize | sha256sum
 fi
