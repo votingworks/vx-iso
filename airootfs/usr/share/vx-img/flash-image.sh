4fe9bc5d2da89da2dbab061dbf0062d2acf7573b
Matt Bernhard <mdb92nc@gmail.com>
2022-04-26 16:44:24 -0400

    Fixing bad merge

--- airootfs/usr/share/vx-img/flash-image.sh
+++ airootfs/usr/share/vx-img/flash-image.sh
@@ -165,17 +165,6 @@ if [ $_hashash == 1 ]; then
     head -c $_finalsize "$_disk" | pv -s $_finalsize | sha256sum
 fi
 
-$_compression -c -d $_path/"$_filename" | pv -s $_finalsize > /dev/nvme0n1
-
-if [ $_hashash == 1 ]; then 
-    echo "Now checking that the write was successful."
-    echo "The hash should be:"
-    cat /usr/share/vx-img/image.sha256sum 
-
-    echo "Computing hash..."
-    head -c $_finalsize /dev/nvme0n1 | pv -s $_finalsize | sha256sum
-fi
-
 # TODO make sure this works on every device
 echo "adding a boot entry for Debian shim"
 efibootmgr \
