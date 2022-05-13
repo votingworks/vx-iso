919bec87338f09bbcfd9101b711ad9060dc1d370
Matt Bernhard <mdb92nc@gmail.com>
2022-05-12 21:12:36 -0400

    fixing decimal point file size bug

--- airootfs/usr/share/vx-img/flash-image.sh
+++ airootfs/usr/share/vx-img/flash-image.sh
@@ -313,16 +313,20 @@ if [[ $answer != 'y' && $answer != 'Y' ]]; then
     exit
 fi
 
-$_compression -c -d $_path/"$_toflash" | pv -s "${_finalsize}" > "$_disk"
+# If the size of the image is decimal, chop off the decimal point and after so
+# pv doesn't choke on it. This is fine, since pv is only using the size to
+# compute status percentages and will still push the whole file to the disk.
+statussize=$(echo "$_finalsize" | cut -d '.' -f 1)
+$_compression -c -d $_path/"$_toflash" | pv -s "${statussize}" > "$_disk"
 
-
+sleep 3
 if [ $_hashash == 1 ]; then 
     echo "Now checking that the write was successful."
     echo "The hash should be:"
     cat /usr/share/vx-img/image.sha256sum 
 
     echo "Computing hash..."
-    head -c $_finalsize "$_disk" | pv -s "${_finalsize}" | sha256sum
+    head -c $_finalsize "$_disk" | pv -s "${statussize}" | sha256sum
 fi
 
 # TODO make sure this works on every device
