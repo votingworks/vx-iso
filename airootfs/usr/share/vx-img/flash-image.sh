2cd7ae8d5517b5779a9c90ab032d5ece39fa2481
Matt Bernhard <umbernhard@gmail.com>
2022-07-22 17:44:26 -0400

    Bernhard/usb keys (#54)
    * Fixing up some things
    
    * bugfixes
    
    * fixing more bugs...:
    
    * trying to remove some repeated code
    
    * More bugfixes, support for lz4 image names
    
    * removing a spurious file
    
    * fixing extension matching issue
    
    * Removing mention of Ventoy
    
    * Cleaning up

--- airootfs/usr/share/vx-img/flash-image.sh
+++ airootfs/usr/share/vx-img/flash-image.sh
@@ -12,6 +12,7 @@ function menu() {
     unset 'items[-1]'
 
     if [ ${#items[@]} -eq 0 ]; then
+        echo "Got an empty list!"
         err=1
         return 
     fi
@@ -27,96 +28,50 @@ function menu() {
     err=0
 }
 
-# This is to evade any race conditions with the display buffer that cuts off
-# text. See votingworks/vx-iso#21.
-sleep 1
-clear
-# use dmidecode to detect if we're on a Surface Go
-if dmidecode | grep -q 'Surface Go'; then
-    _surface=1
-else
-    _surface=0
-fi
+function disk_select() {
+    unset _diskname
+    unset _datadisk
 
-_haskeys=0
-if mokutil --pk > /dev/null; then
-    # Make sure the SB keys are ours
-    # TODO do something fancier once we've decided on our keys
-    _pk=$(mokutil --pk | grep -aq "VotingWorks" && echo 1 || echo 0)
-    _kek=$(mokutil --kek | grep -aq "VotingWorks" && echo 1 || echo 0)
-    _db=$(mokutil --db | grep -aq "VotingWorks" && echo 1 || echo 0)
+    prompt=$1
+    size=$2
 
-    if [[ $_pk == 1 && $_kek == 1 && $_db == 1 ]]; then
-        _haskeys=1
-    fi
-fi
-
-if [[ $_surface == 0  && $_haskeys == 0 ]]; then
-    echo "Writing new secure boot keys to the device. Proceed? [y/N]:"
-
-    read -r answer
-
-    if [[ $answer != 'y' && $answer != 'Y' ]]; then
-        echo "Continue without updating secure boot keys? [y/N]:"
-        read -r answer
-
-        if [[ $answer != 'y' && $answer != 'Y' ]]; then
-            exit
-        fi
+    if [[ -n $size ]]; then
+        readarray disks < <(lsblk -x SIZE -nblo NAME,SIZE,TYPE | grep "disk" | awk -v var="$_size" '$2 > var {print $1,$2}')
+        # This dumps the newlines at the end of the entries in the lsblk table
+        disks=("${disks[@]//$'\n'/}")
+        fixed_disks=()
+        just_disks=()
+        for value in "${disks[@]}"; do
+            name=$(echo "$value" | cut -d ' ' -f 1)
+            size=$(echo "$value" | cut -d ' ' -f 2 | numfmt --to=iec)
+            if [[ "$_datadisk" != *"$name"* ]]; then
+                fixed_disks+=("$name $size")
+                just_disks+=("$name")
+            fi
+        done
+        disks=("${just_disks[@]}")
     else
-        if [[ $_surface == 0 ]]; then
-            _setup=$(mokutil --sb-state | grep -q "Setup Mode")
-
-            if [[ -z $setup ]]; then
-                echo "Device is not in Setup Mode."
-                echo "Please reboot into the BIOS and enter Setup Mode before continuing."
-                echo "Reboot now? [Y/n]:"
-                read -r answer
-
-                if [[ $answer != 'n' && $answer != 'N' ]]; then
-                    systemctl reboot --firmware-setup
-                else
-                    exit
-                fi
-            fi
-        fi
-        SUCCESS=1
-        efi-updatevar -f /etc/efi-keys/DB.auth db && efi-updatevar -f /etc/efi-keys/KEK.auth KEK && efi-updatevar -f /etc/efi-keys/PK.auth PK || SUCCESS=0
-
-        if [[ $SUCCESS != 1 ]]; then
-            echo "Writing the keys failed. Make sure you're in setup mode in your firmware interface. Continue anyways? [y/N]:" 
-            read -r answer
-
-            if [[ $answer != 'y' && $answer != 'Y' ]]; then
-                exit
-            fi
-        fi
+        readarray disks < <(lsblk -x SIZE -nblo NAME,LABEL,SIZE,TYPE | grep "disk" | awk '{ print $1 }')
+        # This dumps the newlines at the end of the entries in the lsblk table
+        disks=("${disks[@]//$'\n'/}")
     fi
-fi
-
-clear
-
-data=$(lsblk -x SIZE -nblo NAME,LABEL,SIZE,TYPE | grep "Data" | awk '{ print $1 }')
-
-if [[ -n $data ]]; then
-    _datadisk="$data"
-    mount "/dev/$_datadisk" /mnt
-else 
-    readarray disks < <(lsblk -x SIZE -nblo NAME,LABEL,SIZE,TYPE | grep "disk" | awk '{ print $1 }')
-    # This dumps the newlines at the end of the entries in the lsblk table
-    disks=("${disks[@]//$'\n'/}")
 
     while true; do 
         # If there's only one disk, no need for a menu
-        if [[ ${#disks[@]} == 1 ]]; then
+        if [[ -z ${disks[0]} ]]; then
+            echo "There are no compatible disks!"
+            err=1
+            return 
+        elif [[ ${#disks[@]} == 1 ]]; then
             _diskname=${disks[0]}
             _datadisk="/dev/${disks[0]}"
+            return
         else
             unset answer
-            menu "${disks[@]}" "Which disk contains the data to flash?"
+            menu "${disks[@]}" "$prompt" 
             if [[ $err == 1 ]]; then
                 echo "Something went wrong. Please try again."
-                exit
+                continue
             fi
 
             if [[ -n $answer ]]; then
@@ -134,8 +89,16 @@ else
                 _diskname=${disks[-1]}
                 _datadisk="/dev/${disks[-1]}"
             fi
+            return
         fi
+    done
+}
 
+function part_select() {
+    unset part
+    _prompt=$2
+    _diskname=$1
+    while true; do
         # Get all the partitions on the selected disk
         readarray parts < <(lsblk -x SIZE -nblo NAME,LABEL,SIZE,TYPE | grep "part" | grep "$_diskname" | awk '{ print $1 }')
         parts=("${parts[@]//$'\n'/}")
@@ -143,12 +106,14 @@ else
         # if there's only one partition, no need for a menu
         if [[ ${#parts[@]} == 1 ]]; then
             part=${parts[0]}
+            return
         else
             unset answer
-            menu "${parts[@]}" "Which partition contains the image?"
+            menu "${parts[@]}" "$_prompt"
             if [[ $err == 1 ]]; then
                 echo "Something went wrong. Please try again."
-                exit
+                err=1
+                return
             fi
 
             if [[ -n $answer ]]; then
@@ -164,9 +129,144 @@ else
             else
                 part=${parts[-1]}
             fi
+            return
         fi
         break
     done
+}
+
+# This is to evade any race conditions with the display buffer that cuts off
+# text. See votingworks/vx-iso#21.
+sleep 1
+clear
+
+function flash_keys() {
+    # use dmidecode to detect if we're on a Surface Go
+    if dmidecode | grep -q 'Surface Go'; then
+        _surface=1
+    else
+        _surface=0
+    fi
+
+    _haskeys=0
+    if mokutil --pk > /dev/null; then
+        # Make sure the SB keys are ours
+        # TODO do something fancier once we've decided on our keys
+        _pk=$(mokutil --pk | grep -aq "VotingWorks" && echo 1 || echo 0)
+        _kek=$(mokutil --kek | grep -aq "VotingWorks" && echo 1 || echo 0)
+        _db=$(mokutil --db | grep -aq "VotingWorks" && echo 1 || echo 0)
+
+        if [[ $_pk == 1 && $_kek == 1 && $_db == 1 ]]; then
+            _haskeys=1
+        fi
+    fi
+
+    if [[ $_surface == 0  && $_haskeys == 0 ]]; then
+        echo "Writing new secure boot keys to the device. Proceed? [y/N]:"
+
+        read -r answer
+
+        if [[ $answer != 'y' && $answer != 'Y' ]]; then
+            echo "Continue without updating secure boot keys? [y/N]:"
+            read -r answer
+
+            if [[ $answer != 'y' && $answer != 'Y' ]]; then
+                exit
+            fi
+        else
+            if [[ $_surface == 0 ]]; then
+                _setup=$(mokutil --sb-state | grep "Setup Mode")
+
+                if [[ -z $_setup ]]; then
+                    echo "Device is not in Setup Mode."
+                    echo "Please reboot into the BIOS and enter Setup Mode before continuing."
+                    echo "Reboot now? [Y/n]:"
+                    read -r answer
+
+                    if [[ $answer != 'n' && $answer != 'N' ]]; then
+                        systemctl reboot --firmware-setup
+                    else
+                        echo "Continue without flashing keys?" 
+                        read -r answer
+
+                        if [[ $answer != 'n' && $answer != 'N' ]]; then
+                            return
+                        else
+                            echo "Please put the machine in Setup Mode before trying again."
+                            exit
+                        fi
+                    fi
+                fi
+            fi
+            SUCCESS=1
+
+            # We have to make sure we can write the keys. 
+            chattr -i /sys/firmware/efi/efivars/db* 2>&1 /dev/null  
+            chattr -i /sys/firmware/efi/efivars/KEK* 2>&1 /dev/null 
+            chattr -i /sys/firmware/efi/efivars/PK* 2>&1 /dev/null 
+
+            keys=$(lsblk -x SIZE -nblo NAME,LABEL,SIZE,TYPE | grep -iF "Keys" | awk '{ print $1 }')
+
+            if [[ -z $keys ]]; then
+                disk_select "Which disk contains the keys to flash?"
+                
+                if [[ $err == 1 ]]; then
+                    echo "Could not select a disk with keys. Not flashing keys."
+                    return
+                else
+                    part_select "$_diskname" "Which partition contains the keys?" 
+
+                    if [[ $err == 1 ]]; then
+                        echo "Could not select a partition with keys. Not flashing keys."
+                        return
+                    fi
+
+                    keys="${part}"
+                fi
+            fi
+            mkdir -p keys
+            mount "/dev/${keys}" keys
+
+
+            efi-updatevar -f keys/DB.auth db && efi-updatevar -f keys/KEK.auth KEK && efi-updatevar -f keys/PK.auth PK || SUCCESS=0
+
+            umount keys
+
+            if [[ $SUCCESS != 1 ]]; then
+                echo "Writing the keys failed. Make sure you're in setup mode in your firmware interface. Continue anyways? [y/N]:" 
+                read -r answer
+
+                if [[ $answer != 'y' && $answer != 'Y' ]]; then
+                    exit
+                fi
+            else
+                echo "Writing keys succeeded!"
+            fi
+        fi
+    fi
+}
+
+flash_keys
+
+clear
+
+data=$(lsblk -x SIZE -nblo NAME,LABEL,SIZE,TYPE | grep -iF "Data" | awk '{ print $1 }')
+
+if [[ -n $data ]]; then
+    _datadisk="$data"
+    mount "/dev/$_datadisk" /mnt
+else
+    disk_select "Which disk contains the image to flash?"
+    if [[ $err == 1 ]]; then
+        echo "Disk selection failed. Could not select an image to flash. Exiting..."
+        exit
+    fi
+
+    part_select "$_diskname" "Which partition contains the image?"
+    if [[ $err == 1 ]]; then
+        echo "Partition selection failed. Could not select an image to flash. Exiting..."
+        exit
+    fi
 
     mount "/dev/${part}" /mnt
 fi
@@ -174,13 +274,13 @@ fi
 
 clear
 if [[ -n $data ]]; then 
-    echo "Found Ventoy Data partition ${_datadisk} and mounted on /mnt"
+    echo "Found data partition ${_datadisk} and mounted on /mnt"
 else
-    echo "Mounted data disk ${_datadisk}${part} on /mnt"
+    echo "Mounted data disk ${part} on /mnt"
 fi
 
 # Expected file naming scheme
-_match="^\d+(\.\d)*G-\d{4}-\d{2}-\d{2}T\d{2}(:|_|\s)\d{2}(:|_|\s)\d{2}(\+|-)\d{2}(:|_|\s)\d{2}-.*\.img\.gz$"
+_match="^\d+(\.\d)*G-\d{4}-\d{2}-\d{2}T\d{2}(:|_|\s)\d{2}(:|_|\s)\d{2}(\+|-)\d{2}(:|_|\s)\d{2}-.*\.img\.(gz|lz4)$"
 _sizematch="^\d+(\.\d)*G"
 
 
@@ -193,6 +293,7 @@ _images=()
 _extensions=()
 
 _matches=()
+_matchextensions=()
 
 for f in "$_path"/*; do
     _filename="${f##*/}"
@@ -200,6 +301,7 @@ for f in "$_path"/*; do
 
     if (echo "$_filename" | grep -qPo "$_match") ; then
         _matches+=("$_filename")
+        _matchextensions+=("$_extension")
     elif [[ "$_extension" == "gz" || "$_extension" == "lz4" ]]; then
         _images+=("$_filename")
         _extensions+=("$_extension")
@@ -216,7 +318,7 @@ fi
 if [[ "${#_matches[@]}" == 1 ]]; then
     echo "Found only one image in the right format."
     _toflash=${_matches[0]}
-    _extension="gz"
+    _extension=${_matchextensions[0]}
     _finalsize=$(echo "$_toflash" | grep -oP "$_sizematch")
 elif [[ -n ${_matches[0]} ]]; then
     echo "Found several images that match the expected format."
@@ -233,7 +335,7 @@ elif [[ -n ${_matches[0]} ]]; then
     else
         _toflash=${_matches[-1]}
     fi
-    _extension="gz"
+    _extension=${_matchextensions[0]}
     _finalsize=$(echo "$_toflash" | grep -oP "$_sizematch")
 elif [[ "${#_images[@]}" == 1 ]]; then
     echo "Found only one image that might work."
@@ -283,54 +385,18 @@ _disk="/dev/nvme0n1"
 _size=$(numfmt --from=iec "${_finalsize}")
 
 clear
-echo "Found the following disks to flash:"
-while true; do
+disk_select "Which disk would you like to flash?" "$_size"
 
-    # Get a list of all available disks large enough to take our image, sorted by size
-    readarray disks < <(lsblk -x SIZE -nblo NAME,SIZE,TYPE | grep "disk" | awk -v var="$_size" '$2 > var {print $1,$2}')
-    # dump newlines
-    disks=("${disks[@]//$'\n'/}")
+if [[ $err == 1 ]]; then
+    echo "No disks were big enough for the image! Exiting..."
+    exit
+fi
 
-    # remove the data disk, since we don't want to flash it.
-    fixed_disks=()
-    for value in "${disks[@]}"; do
-        name=$(echo "$value" | cut -d ' ' -f 1)
-        size=$(echo "$value" | cut -d ' ' -f 2 | numfmt --to=iec)
-        if [[ "$_datadisk" != *"$name"* ]]; then
-            fixed_disks+=("$name $size")
-        fi
-    done
-
-    if [[ -z ${disks[0]} ]]; then
-        echo "There are no disks big enough for this image! Exiting..."
-        exit
-    elif [[ "${#fixed_disks[@]}" == 1 ]]; then
-        _disk="/dev/$(echo "${fixed_disks[0]}" | cut -d ' ' -f 1)"
-    else 
-        unset answer
-        menu "${fixed_disks[@]}" "Which disk would you like to flash?" 
-
-        if [[ $err == 1 ]]; then
-            echo "Something went wrong. Please try again."
-            continue
-        fi 
-
-        if [[ -n $answer ]]; then
-            selected=$(echo "${fixed_disks[answer-1]}" | cut -d ' ' -f 1)
-
-            if [[ -z $selected ]]; then
-                echo "Invalid selection, starting over"
-                continue
-            fi
-            _disk="/dev/$selected"
-        else
-            _disk="/dev/$(echo "${fixed_disks[-1]}" | cut -d ' ' -f 1)"
-        fi
-    fi
-    break
-done
-
-echo "Flashing image $_path/$_toflash to disk $_disk. Continue? [y/N]"
+echo "Flashing image" 
+echo "$_path/$_toflash"
+echo "to disk"
+echo "$_datadisk"
+echo "Continue? [y/N]"
 
 read -r answer
 
@@ -345,7 +411,7 @@ statussize="$(echo "$_finalsize" | cut -d '.' -f 1)"
 if ! (echo "$statussize" | grep -qo "G"); then
     statussize="${statussize}G"
 fi 
-$_compression -c -d $_path/"$_toflash" | pv -s "${statussize}" > "$_disk"
+$_compression -c -d $_path/"$_toflash" | pv -s "${statussize}" > "$_datadisk"
 
 sleep 3
 if [ $_hashash == 1 ]; then 
@@ -354,14 +420,14 @@ if [ $_hashash == 1 ]; then
     cat /usr/share/vx-img/image.sha256sum 
 
     echo "Computing hash..."
-    head -c $_finalsize "$_disk" | pv -s "${statussize}" | sha256sum
+    head -c $_finalsize "$_datadisk" | pv -s "${statussize}" | sha256sum
 fi
 
 # TODO make sure this works on every device
 echo "adding a boot entry for Debian shim"
 efibootmgr \
 	--create \
-	--disk "$_disk" \
+	--disk "$_datadisk" \
 	--part 1 \
 	--label "grub" \
 	--loader "\\EFI\\debian\\shimx64.efi" \
@@ -371,7 +437,7 @@ efibootmgr \
 echo "adding a boot entry for VxLinux"
 efibootmgr \
 	--create \
-	--disk "$_disk" \
+	--disk "$_datadisk" \
 	--part 1 \
 	--label "VxLinux" \
 	--loader "\\EFI\\debian\\VxLinux-signed.efi" \
