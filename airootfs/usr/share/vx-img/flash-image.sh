844e83c24f85412ffc7c929d048c15ed0a593da7
Matt Bernhard <umbernhard@gmail.com>
2022-08-02 18:53:00 -0400

    Fixing pathing/filename for hash files (#62)

--- airootfs/usr/share/vx-img/flash-image.sh
+++ airootfs/usr/share/vx-img/flash-image.sh
@@ -436,7 +436,7 @@ sleep 3
 if [ $_hashash == 1 ]; then 
     echo "Now checking that the write was successful."
     echo "The hash should be:"
-    cat /usr/share/vx-img/image.sha256sum 
+    cat "${_path}/${_toflash}.sha256sum"
 
     echo "Computing hash..."
     head -c $_finalsize "$_datadisk" | pv -s "${statussize}" | sha256sum
