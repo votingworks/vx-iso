ed00a828ab784aefee1d50fd5e6a3c4e57608848
Adam McManus <adam.mcmanus@gmail.com>
2024-12-11 12:41:09 -0600

    bring over existing functionality

--- mkosi/mkosi.extra/usr/local/bin/boot-menu.sh
+++ mkosi/mkosi.extra/usr/local/bin/boot-menu.sh
@@ -5,42 +5,53 @@ clear
 
 while true; do
   choice=$( dialog --clear --title "VX-ISO Menu" \
-	  --menu "Please selection an option: " 15 50 5 \
-	  1 "Open Console" \
-	  2 "Show the date" \
-	  3 "Reboot to BIOS" \
-	  4 "Shutdown" \
-	  5 "Install Image" \
+	  --menu "Please select an option: " 15 50 7 \
+	  1 "Install Image" \
+	  2 "Open Console" \
+	  3 "Compute System Hash" \
+	  4 "Delete Boot Entries" \
+	  5 "Reboot to BIOS" \
+	  6 "Shutdown" \
+	  7 "Scrape Image" \
 	  2>&1 >/dev/tty)
 
   clear
 
   case $choice in
 	1) 
+	   echo "Installing image..."
+	   sleep 2
+	   /usr/local/bin/flash-image.sh
+	   ;;
+	2) 
 	   echo "Starting console..."
 	   sleep 2
 	   exit 0
 	   ;;
-	2) 
-	   echo "Show the date..."
-	   sleep 2
-	   date
-	   sleep 10
-	   ;;
 	3) 
+	   echo "Calculating Hash..."
+	   sleep 2
+	   /usr/local/bin/compute-system-hash.sh
+	   ;;
+	4) 
+	   echo "Opening session to delete boot entries..."
+	   sleep 2
+	   /usr/local/bin/delete-boot-entries.sh
+	   ;;
+	5) 
 	   echo "Rebooting to BIOS..."
 	   sleep 2
 	   systemctl reboot --firmware
 	   ;;
-	4) 
+	6) 
 	   echo "Shutting down..."
 	   sleep 2
 	   systemctl poweroff
 	   ;;
-	5) 
-	   echo "Installing image..."
+	7) 
+	   echo "Scraping image..."
 	   sleep 2
-	   /usr/local/bin/flash-image.sh
+	   /usr/local/bin/scrape-image.sh
 	   ;;
   esac
 done
---
+++ mkosi/mkosi.extra/usr/local/bin/compute-system-hash.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "TODO: Implement verify hash generation from vx-iso against installed image"
+
+sleep 10
+
+exit 0
+
+#VERITY_HASH="$(cat /proc/cmdline | awk -F'verity.hash=' '{print $2}' | cut -d' ' -f1)"
+
+#if [[ ! -z "${VERITY_HASH}" ]]; then
+    #echo "${VERITY_HASH}"
+#else
+    #echo "UNVERIFIED"
+#fi
