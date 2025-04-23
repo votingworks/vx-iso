6b82e8312657a2a5ac9cf93a208e563f4b328b16
Adam McManus <adam.mcmanus@gmail.com>
2025-04-23 17:01:47 -0500

    check for the signed version of our efi, only compute the hash if it's found

--- live-build/includes.chroot/usr/local/bin/compute-system-hash.sh
+++ live-build/includes.chroot/usr/local/bin/compute-system-hash.sh
@@ -2,14 +2,19 @@
 
 trap '' SIGINT SIGTSTP SIGTERM
 
+set -euo pipefail
+
 #hardcoded to nvme for now
 mount -o ro /dev/nvme0n1p1 /mnt
 
 # Check for the VxLinux-signed.efi and handle if it doesn't exist
-# Not dealing with it for now
 # That aside, we're using the strings command to extract the verity hash
 # from the signed efi binary to later use when calculating the SHV
-VERITY_HASH=$(strings /mnt/EFI/debian/VxLinux-signed.efi | grep -o verity.hash=[a-zA-Z0-9]* | cut -d'=' -f2)
+if [[ -f /mnt/EFI/debian/VxLinux-signed.efi ]]; then
+  VERITY_HASH=$(strings /mnt/EFI/debian/VxLinux-signed.efi | grep -o verity.hash=[a-zA-Z0-9]* | cut -d'=' -f2)
+else
+  VERITY_HASH=""
+fi
 
 umount /mnt
 
