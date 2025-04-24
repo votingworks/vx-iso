3129c119e3c2a1c5598f3857f6697408af14c3ed
Adam McManus <adam.mcmanus@gmail.com>
2025-04-24 12:53:23 -0500

    Update SHV script (#106)
    * update shv to be base64 encoded and prep for a qr code version
    
    * add comments related to a future QR code integration, not necessary for the initial release
    
    * check for the signed version of our efi, only compute the hash if it's found
    
    * use system hash instead of signed hash, handle case where xxd or base64 tools are not available
    
    * remove conditional logic since are guaranteed to have the xxd and base64 tools installed now

--- live-build/includes.chroot/usr/local/bin/compute-system-hash.sh
+++ live-build/includes.chroot/usr/local/bin/compute-system-hash.sh
@@ -4,16 +4,32 @@ trap '' SIGINT SIGTSTP SIGTERM
 
 set -euo pipefail
 
-echo "TODO: Implement verify hash generation from vx-iso against installed image"
+#hardcoded to nvme for now
+mount -o ro /dev/nvme0n1p1 /mnt
 
-sleep 10
+# Check for the VxLinux-signed.efi and handle if it doesn't exist
+# That aside, we're using the strings command to extract the verity hash
+# from the signed efi binary to later use when calculating the SHV
+if [[ -f /mnt/EFI/debian/VxLinux-signed.efi ]]; then
+  VERITY_HASH=$(strings /mnt/EFI/debian/VxLinux-signed.efi | grep -o verity.hash=[a-zA-Z0-9]* | cut -d'=' -f2)
+else
+  VERITY_HASH=""
+fi
+
+umount /mnt
+
+if [[ ! -z "${VERITY_HASH}" ]]; then
+  base64_hash=$( echo -n ${VERITY_HASH} | xxd -r -p | base64 )
+  echo "System Hash: ${base64_hash}"
+  read -p "Press Enter once you have validated the System Hash."
+else
+    echo "System Hash: UNVERIFIED"
+    read -p "This is not a verified image. Press Enter to continue."
+fi
+
+# TODO/Future: Add QR code support that can be verified at check.voting.works
+# qrencode -t ANSI "payload" -o -
+# where payload is the properly formatted and signed payload
+#
 
 exit 0
-
-#VERITY_HASH="$(cat /proc/cmdline | awk -F'verity.hash=' '{print $2}' | cut -d' ' -f1)"
-
-#if [[ ! -z "${VERITY_HASH}" ]]; then
-    #echo "${VERITY_HASH}"
-#else
-    #echo "UNVERIFIED"
-#fi
