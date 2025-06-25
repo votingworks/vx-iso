40a44c3e9bceaf02636940094e30e521030d399c
Adam McManus <adam.mcmanus@gmail.com>
2025-06-25 11:18:44 -0500

    add a bit of disk size logic

--- live-build/includes.chroot/usr/local/bin/zero-emmc.sh
+++ live-build/includes.chroot/usr/local/bin/zero-emmc.sh
@@ -12,7 +12,14 @@ device_path="/dev/mmcblk0"
 if [[ -b "${device_path}" ]]; then
   echo "Found a valid block device at ${device_path}."
   echo "The device will be zeroed out. This will take some time."
-  dd if=/dev/zero of="${device_path}" bs=8M status=progress
+  # We're going to use a bs=4M option with dd, so calculate how
+  # many blocks are needed. This is technically unnecessary
+  # because we could let dd write until it runs out of space,
+  # but I don't like the error message, so here we are.
+  device_size=$( blockdev --getsize64 ${device_path} )
+  num_blocks=$(( device_size / 1048576 / 4))
+
+  dd if=/dev/zero of="${device_path}" bs=4M count="${num_blocks}" conv=fsync status=progress
   echo "The ${device_path} device has been zeroed out."
   sleep 2
   exit 0
